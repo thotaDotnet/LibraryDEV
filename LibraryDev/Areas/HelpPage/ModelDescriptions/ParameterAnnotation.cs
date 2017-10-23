6e98968c3265b4fc57f89f5486c2920a22f0db5c
thotaDotnet <thota.assp.net@gmail.com>
2017-10-23 19:44:23 -0400

    Add missed  class

---
+++ LibraryDev/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LibraryDev.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
